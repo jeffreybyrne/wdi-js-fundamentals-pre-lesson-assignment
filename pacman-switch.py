4c6737e9be25ff920c2d2a5380be95af579332bc
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-21 14:28:30 -0400

    finished the exercises

---
+++ pacman-switch.py
@@ -0,0 +1,11 @@
+ghost = 'Pinky'
+if ghost == 'Inky':
+    colour = 'Cyan'
+elif ghost == 'Blinky':
+    colour = 'Red'
+elif ghost == 'Pinky':
+    colour = 'Pink'
+elif ghost == 'Clyde':
+    colour = 'Orange'
+
+print(f"{ghost} is the colour {colour}.")
